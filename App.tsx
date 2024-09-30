55ebb0a9e5f6e47600513be973127be97e7f55a0
ivan <van8tka@tut.by>
2024-09-30 20:32:24 +0300

    feat: added GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import React, {useEffect} from 'react';
-import {Platform, View, Image} from 'react-native';
+import {Platform, View, Image, StyleSheet} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  ComposedGesture, Gesture,
+  GestureDetector,
+  GestureHandlerRootView, GestureType
+} from "react-native-gesture-handler";
 
 function App(): React.JSX.Element {
   useEffect(() => {
@@ -10,21 +15,52 @@ function App(): React.JSX.Element {
       : setTimeout(() => SplashScreen.hide(), 1000);
   }, []);
 
+  function onBeginGestureHandler() {
+    console.log('begin');
+  }
+
+  function onTouchesMoveGestureHandler() {
+    console.log('move');
+  }
+
+  function onEndGestureHandler() {
+    console.log('end');
+  }
+
+  const handleGesture = Gesture.Tap()
+    .onBegin(onBeginGestureHandler)
+    .onTouchesMove(onTouchesMoveGestureHandler)
+    .onEnd(onEndGestureHandler);
+
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        height: '100%',
-        width: '100%',
-        backgroundColor: Colors.gray,
-      }}>
-      <Image
-        source={require('./assets/images/zhunya.png')}
-        style={{height: '100%', width: '100%'}}
-        resizeMode="cover"
-      />
-    </View>
+    <GestureHandlerRootView style={styles.gesture}>
+      <View style={styles.container}>
+        <GestureDetector gesture={handleGesture}>
+          <Image
+            source={require('./assets/images/zhunya.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  gesture: {
+    flex: 1,
+  },
+  container: {
+    flexDirection: 'row',
+    height: '100%',
+    width: '100%',
+    backgroundColor: Colors.gray,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
 export default App;
