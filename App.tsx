9df3c0ba2862d1fd856d838c799dfb46d0e10fda
ivan <van8tka@tut.by>
2024-10-01 09:50:54 +0300

    feat: added Sound rn-package and mp3 file

--- App.tsx
+++ App.tsx
@@ -1,34 +1,64 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Platform, View, Image, StyleSheet} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {
-  ComposedGesture, Gesture,
+  Gesture,
   GestureDetector,
-  GestureHandlerRootView, GestureType
-} from "react-native-gesture-handler";
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import Sound from 'react-native-sound';
+
+Sound.setCategory('Playback');
+
+const audio = new Sound(require('./assets/media/myr.mp3'), error => {
+  if (error) {
+    console.log('failed to load the sound', error);
+    return;
+  }
+
+  console.log('sound loaded');
+});
 
 function App(): React.JSX.Element {
   useEffect(() => {
     Platform.OS === 'ios'
       ? SplashScreen.hide()
       : setTimeout(() => SplashScreen.hide(), 1000);
-  }, []);
 
-  function onBeginGestureHandler() {
-    console.log('begin');
-  }
+    audio.setVolume(1);
+
+    return () => {
+      console.log('release media');
+      audio.release();
+    };
+  }, []);
 
   function onTouchesMoveGestureHandler() {
     console.log('move');
+    startPlayMediaHandler();
   }
 
   function onEndGestureHandler() {
     console.log('end');
+    endPlayMediaHandler();
+  }
+
+  function startPlayMediaHandler() {
+    audio.play(success => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  }
+
+  function endPlayMediaHandler() {
+    audio.stop();
   }
 
   const handleGesture = Gesture.Tap()
-    .onBegin(onBeginGestureHandler)
     .onTouchesMove(onTouchesMoveGestureHandler)
     .onEnd(onEndGestureHandler);
 
